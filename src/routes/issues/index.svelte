3cd17ac373f2df41187f7235bc197c6448517c3a
Jan Hovancik <conta.srdr@gmail.com>
2020-01-01 19:53:01 +0100

    First issue and related pages for "Issues"

---
+++ src/routes/issues/index.svelte
@@ -0,0 +1,30 @@
+<script context="module">
+
+  export function preload({ params, query }) {
+    return this.fetch(`issues.json`).then(r => r.json()).then(posts => {
+      return { posts };
+    });
+  }
+</script>
+
+<script>
+  import Issue from '../../components/Issue.svelte';
+  import Meta from '../../components/Meta.svelte';
+  export let posts;
+  const meta = {
+    name: "BSD Weekly - Issues",
+    description: "Free, once–weekly e-mail round-up of BSD news and articles.",
+    url: "https://bsdweekly.com/issues"
+  }
+</script>
+
+<svelte:head>
+  <title>{meta.name}</title>
+  <Meta {...meta}/>
+</svelte:head>
+
+<div>
+  {#each posts as post}
+    <Issue {post}/>
+  {/each}
+</div>
---
+++ src/routes/issues/latest.json.js
@@ -0,0 +1,11 @@
+import posts from './_posts.js';
+
+const contents = JSON.stringify(posts[0]);
+
+export function get(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+
+  res.end(contents);
+}

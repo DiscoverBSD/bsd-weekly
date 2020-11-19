429183395c0facb81b424ece8dac8b03cba44bed
Jan Hovancik <conta.srdr@gmail.com>
2020-11-19 16:46:02 +0100

    Upgrade CSS

--- src/components/Subscribe.svelte
+++ src/components/Subscribe.svelte
@@ -4,7 +4,7 @@
 		<input type="email" value="" name="EMAIL" class="appearance-none text-gray-700 border border-red-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-red-500" id="mce-EMAIL" placeholder="Your email address" required>
 		<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 		<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7ce75bb5ad016b05238a9a095_fcd091cb1d" tabindex="-1" value=""></div>
-  	<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="shadow bg-red-800 hover:bg-red-700 focus:shadow-outline focus:outline-none text-white font-bold text-2xl py-3 px-5 rounded">
+  	<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="shadow bg-red-800 hover:bg-red-700 focus:ring focus:outline-none text-white font-bold text-2xl py-3 px-5 rounded">
 		<p class="text-sm my-2 text-gray-600">We won't spam you. Unsubscribe any time.</p>
   </div>
 </form>

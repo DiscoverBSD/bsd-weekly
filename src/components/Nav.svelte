31e3566163bd3a1a58f93806b14bc7be9d3e4093
Jan Hovancik <jan@hovancik.net>
2025-02-13 10:00:06 +0000

    Fix nav
    closes Links on HP not working correctly #260

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,26 +1,42 @@
 <script>
-	import Subscribe from '../components/Subscribe.svelte';
+	import Subscribe from "../components/Subscribe.svelte";
 </script>
 
 <nav class="py-2">
 	<ul class="float-right flex flex-row text-lg">
 		<li class="mr-6">
-			<a class="text-red-800 hover:text-red-700" href='.'>LATEST</a>
+			<a class="text-red-800 hover:text-red-700" href="/">LATEST</a>
 		</li>
 		<li class="mr-6">
-			<a class="text-red-800 hover:text-red-700" rel=prefetch href='issues'>ARCHIVES</a>
+			<a class="text-red-800 hover:text-red-700" rel="prefetch" href="/issues"
+				>ARCHIVES</a
+			>
 		</li>
 	</ul>
-	<a href="." class="text-4xl text-red-800 hover:text-red-700 font-base leading-tight mb-4">BSD Weekly</a>
+	<a
+		href="/"
+		class="text-4xl text-red-800 hover:text-red-700 font-base leading-tight mb-4"
+		>BSD Weekly</a
+	>
 	<div class="text-center text-xl text-gray-800">
 		<p class="mt-8">
-			BSD Weekly is a free, once–weekly e-mail round-up of BSD news and articles.<br/>
-			Curated by <a class="text-red-800 hover:text-red-700" href="https://hovancik.net">Jan Hovancik</a>
-			from your content on <a class="text-red-800 hover:text-red-700" href="https://discoverbsd.com">DiscoverBSD</a>
-			and <a class="text-red-800 hover:text-red-700" href="https://bsdsec.net">BSDSec</a>.
+			BSD Weekly is a free, once–weekly e-mail round-up of BSD news and
+			articles.<br />
+			Curated by
+			<a class="text-red-800 hover:text-red-700" href="https://hovancik.net"
+				>Jan Hovancik</a
+			>
+			from your content on
+			<a class="text-red-800 hover:text-red-700" href="https://discoverbsd.com"
+				>DiscoverBSD</a
+			>
+			and
+			<a class="text-red-800 hover:text-red-700" href="https://bsdsec.net"
+				>BSDSec</a
+			>.
 		</p>
 		<div class="my-6">
-			<Subscribe/>
+			<Subscribe />
 		</div>
 	</div>
 </nav>
